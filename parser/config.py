0e686dd079f0aaff34097e2d8e4325091ba4fb41
naum <yehornaumenko@gmail.com>
2019-11-17 20:53:13 +0200

    Output's been improved

--- parser/config.py
+++ parser/config.py
@@ -1,4 +1,7 @@
-city_link = "https://m.vkino.ua/ru/afisha/kharkov"
+links_navigator = {
+    "main": "https://m.vkino.ua",
+    "board": "/ru/afisha/kharkov"
+}
 headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0)'
                       ' Gecko/20100101 Firefox/45.0'
--- parser/main.py
+++ parser/main.py
@@ -1,5 +1,5 @@
 import requests
-import config
+import config as cfg
 import datetime
 import bs4
 
@@ -10,26 +10,29 @@ def return_movies(required_date=datetime.date.today()):
         'month': required_date.strftime('%m'),
         'day': required_date.strftime('%d')
     }
-    params = {'date': '{}-{}-{}'.format(date['year'],
-                                        date['month'],
-                                        date['day'])}
+    params = {'date': '{}-{}-{}'.format(date.get('year'),
+                                        date.get('month'),
+                                        date.get('day'))}
     classes = {'common': 'film-box-holder actual',
                'personal': 'film-box',
                'title': 'film-title',
                'link': 'btn-buy'}
+    board_link = \
+        cfg.links_navigator.get('main') + cfg.links_navigator.get('board')
 
-    r = requests.get(url=config.city_link,
+    r = requests.get(url=board_link,
                      params=params,
-                     headers=config.headers)
+                     headers=cfg.headers)
     page = bs4.BeautifulSoup(r.text, features='html.parser')
     only_films = \
-        page.find('div', classes['common']).find_all('div',
-                                                     classes['personal'])
+        page.find('div', classes.get('common')).find_all('div',
+                                                         classes.get('personal'))
     for i in range(len(only_films)):
         film_name = \
-            only_films[i].find('a', classes['title']).find('span').get_text()
-        info_link = \
-            only_films[i].find('a', classes['link']).get("href")
+            only_films[i].find('a', classes.get('title')).find('span').get_text()
+        info_link = '{}{}'.format(
+            cfg.links_navigator.get('main'),
+            only_films[i].find('a', classes.get('link')).get('href'))
         only_films[i] = {film_name: info_link}
 
     return only_films
