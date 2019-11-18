3dabbc51cbec40d8a10f64e76739ec2b2ad95759
naum <yehornaumenko@gmail.com>
2019-11-18 13:17:13 +0200

    Getting of detailed description

--- parser/main.py
+++ parser/main.py
@@ -25,12 +25,12 @@ def return_movies(required_date=datetime.date.today()):
                      params=params,
                      headers=cfg.headers)
     page = bs4.BeautifulSoup(r.text, features='html.parser')
-    only_films = \
-        page.find('div', classes.get('common')).find_all('div',
-                                                         classes.get('personal'))
+    only_films = page.find('div', classes.get('common')
+                           ).find_all('div',
+                                      classes.get('personal'))
     for i in range(len(only_films)):
-        film_name = \
-            only_films[i].find('a', classes.get('title')).find('span').get_text()
+        film_name = only_films[i].find('a', classes.get('title')
+                                       ).find('span').get_text()
         info_link = '{}{}'.format(
             cfg.links_navigator.get('main'),
             only_films[i].find('a', classes.get('link')).get('href'))
@@ -39,4 +39,28 @@ def return_movies(required_date=datetime.date.today()):
     return only_films
 
 
-print(return_movies())
+def info_about(info_link):
+    classes = {
+        'description': {'element': 'div', 'class': 'film-description'},
+        'title': {'element': 'h3', 'class': 'title'},
+        'translation': {'element': 'span', 'class': 'sub-title'}
+    }
+
+    r = requests.get(info_link)
+    page = bs4.BeautifulSoup(r.text, features='html.parser')
+    film_data = {
+        'title':
+            page.find(classes.get('description').get('element'),
+                      classes.get('description').get('class')
+                      ).find('h3', {'class': 'title'}).get_text(),
+        'translation':
+            page.find(classes.get('description').get('element'),
+                      classes.get('description').get('class')
+                      ).find(classes.get('translation').get('element'),
+                             classes.get('translation').get('class')
+                             ).get_text()
+    }
+    return film_data
+
+
+print(info_about('https://m.vkino.ua/ru/show/8010-angeli-charli/kharkov?date=2019-11-18'))
