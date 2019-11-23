aa405156c9ea61ed1f62c9a0c1b7e7d729618582
Omelman <54813861+Omelman@users.noreply.github.com>
2019-11-23 18:03:30 +0200

    Add files via upload

---
+++ request.py
@@ -0,0 +1,29 @@
+import null as null
+from flask import Flask
+import main as man
+import datetime
+import json
+
+app: Flask = Flask(__name__)
+info = {}
+
+
+@app.route('/general', methods=['GET'])
+def general(date=datetime.date.today()):
+    data = man.return_movies(date)
+    r = 0
+    for k, v in data.items():
+        info[r] = (man.info_about(v))
+        s = (man.get_mdb_info(info[r].get('translation')))
+        if s.get('imdbRating') == "N/A" or s.get('imdbRating') is None:
+            info[r]['imdb'] = ''
+        else:
+            info[r]['imdb'] = s.get('imdbRating')
+        r = r + 1
+    json_info = json.dumps(info, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
+    return json_info
+
+
+@app.route('/details', methods=['GET'])
+def details(name=None):
+    return man.get_mdb_info(name)
